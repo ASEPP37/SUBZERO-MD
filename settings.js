9bd04b5204073e297459f5b00e42f5caaa0b4f9e
ASEPP37 <abeyapaps@gmail.com>
2025-10-07 01:33:39 +0100

    Update settings.js for Bitbot

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;mJ0HEb6K#79mptjMNQe0b8kN5ZuUmi8hSmewariKj9bZkgyAAm-0",
+  OWNER_NUMBER: "+237694838613", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
